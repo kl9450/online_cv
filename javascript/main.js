21607b6c9d34756243e9b4d0426d06b21244ff91
kl9450 <plamediluyeye@gmail.com>
2018-11-26 16:22:13 +0200

    eddited nav bar

--- javascript/main.js
+++ javascript/main.js
@@ -1,3 +1,16 @@
+window.onscroll = function() {myFunction()};
+var navbar = document.getElementById("navbar");
+var sticky = navbar.offsetTop;
+
+function myFunction() {
+  if (window.pageYoffset >= sticky) {
+    navbar.classList.add("sticky")
+  } else {
+    navbar.classList.remove("sticky")
+  }
+}
+
+// Image slider object
 (function () {
  
   const sliderModel = {
