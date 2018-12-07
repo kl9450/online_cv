1f0605c38e85ca43e1f4b4b2876b766ae50c7336
kl9450 <plamediluyeye@gmail.com>
2018-12-07 14:28:19 +0200

    added media queries

--- javascript/main.js
+++ javascript/main.js
@@ -1,5 +1,12 @@
-var name = prompt("Enter your name")
-function greetingUser() {
-}
+// var name = prompt("Enter your name")
+// function greetingUser() {
+// }
 
-alert("Welcome " + name);
+// alert("Welcome " + name);
+
+let mainNav = document.getElementById('js-menu');
+let navBarToggle = document.getElementById('js-navbar-toggle');
+
+navBarToggle.addEventListener ('click', function (){
+    mainNav.classList.toggle('active');
+});
